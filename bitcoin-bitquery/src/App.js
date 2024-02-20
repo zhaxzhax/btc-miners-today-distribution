7f3da1f30c43f7ad28892f4be36e7d65ec4db6c5
zhaxzhax <hizhaxzhax@gmail.com>
2024-02-20 12:50:08 -0800

    init today's block information

---
+++ bitcoin-bitquery/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import BlockStats from './BlockStats';
+
+function App() {
+  return (
+    <div className="App">
+      <BlockStats></BlockStats>
+    </div>
+  );
+}
+
+export default App;
